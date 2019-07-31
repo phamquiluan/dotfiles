fcb3e4c2206c37f8c49d687a3dcb45f85676b3aa
Luan Pham <phamquiluan@gmail.com>
2019-07-31 18:48:43 +0700

    Cd to current dir before install pip

--- init.sh
+++ init.sh
@@ -1,16 +1,23 @@
 #!/bin/sh
 
+cur_dir="$(pwd)"
+echo "Copying source files..."
 cp bashrc ~/.bashrc
 cp vimrc ~/.vimrc
 cp ycm_extra_conf.py ~/.ycm_extra_conf
 
+echo "Setup Vim, YouCompleteMe"
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
 cd ~/.vim/bundle/YouCompleteMe
 python3 install.py
 
+cd $cur_dir
 input='requirements.txt'
 while IFS= read -r line
 do
 	pip install "$line"
 done < "$input"
+echo "********************************"
+echo "\tTee is my hero!!"
+echo "********************************"
