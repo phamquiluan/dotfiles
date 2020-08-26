ea59f3b598d321236ba79335a20f9b582f073e23
Luan Pham <phamquiluan@gmail.com>
2020-08-26 17:04:22 +0700

    sdfsdf

--- init.sh
+++ init.sh
@@ -16,6 +16,7 @@ cp .bashrc ~/.bashrc
 cp .bash_aliases ~/.bash_aliases
 cp .vimrc ~/.vimrc
 cp .ycm_extra_conf.py ~/.ycm_extra_conf
+cp .inputrc ~/.inputrc
 
 echo "Setup Vim, YouCompleteMe"
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
