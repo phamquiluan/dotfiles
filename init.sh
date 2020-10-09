39a82faaf7e27d8e8ccaa78e24b9151f4b2bdc75
Luan Pham <luan@cinnamon.is>
2020-10-10 01:57:52 +0700

    Install vim first

--- init.sh
+++ init.sh
@@ -3,10 +3,8 @@
 if [ ! -d "$HOME/anaconda3" ] || [ ! -d "$HOME/miniconda3" ] ;
 then
 	echo "Conda could not be found! Installing conda.."
-	# wget "https://repo.continuum.io/archive/Anaconda3-5.3.1-Linux-x86_64.sh"
 	wget "https://repo.anaconda.com/miniconda/Miniconda3-py37_4.8.3-Linux-x86_64.sh"
 	bash Miniconda3-py37_4.8.3-Linux-x86_64.sh -b -p "$HOME/miniconda3"
-	# bash Anaconda3-5.3.1-Linux-x86_64.sh -b -p $HOME/anaconda3
 else
 	echo "Conda is found!!!"
 fi
@@ -14,9 +12,10 @@ fi
 
 cur_dir="$(pwd)"
 
-echo "=== Installing build-essential cmake vim.. ==="
+echo "===== Installing build-essential cmake vim.. ====="
 if [ command -v apt &> /dev/null ];
 then 
+	sudo add-apt-repository ppa:jonathonf/vim
 	sudo apt update -y	
 	sudo apt install -y build-essential cmake vim-gtk
 else
@@ -24,7 +23,7 @@ else
 	sudo yum install -y build-essential cmake vim-gtk
 fi
 
-echo "Copying source files..."
+echo "===== Copying config files.. ====="
 cp .bashrc ~/.bashrc
 cp .bash_aliases ~/.bash_aliases
 cp .vimrc ~/.vimrc
@@ -32,7 +31,7 @@ cp .ycm_extra_conf.py ~/.ycm_extra_conf
 cp .inputrc ~/.inputrc
 cp .tmux.conf ~/.tmux.conf
 
-echo "Setup Vim, YouCompleteMe"
+echo "===== Setup YouCompleteMe.. ====="
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
 cd ~/.vim/bundle/YouCompleteMe
@@ -40,11 +39,11 @@ python3 install.py --clang-completer
 
 
 cd $cur_dir
-echo "copy vim template"
+echo "===== Copy vim template ====="
 cp -r templates ~/.vim/templates
 
 
-echo "Install fzf"
+echo "===== Install fzf.. ====="
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 bash ~/.fzf/install --all
 
