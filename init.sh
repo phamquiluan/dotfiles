02350da08d334bd20d7f8e786c5afbd4c7ddd0dd
Luan Pham <luan@cinnamon.is>
2020-10-10 02:37:31 +0700

    remove clang completer

--- init.sh
+++ init.sh
@@ -39,7 +39,7 @@ echo "===== Setup YouCompleteMe.. ====="
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
 cd ~/.vim/bundle/YouCompleteMe
-python3 install.py --clang-completer
+python3 install.py
 
 
 cd $CUR_DIR
