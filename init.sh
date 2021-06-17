027379b3ddae9c374bb397377dd71f1456e3d5f4
Luan Pham <luan@cinnamon.is>
2021-06-17 23:00:16 +0700

    Add pre-commit and compile vim from source

--- init.sh
+++ init.sh
@@ -4,26 +4,29 @@ CUR_DIR="$(pwd)"
 
 APT_CMD=$(which apt)
 
-printf "\n===== Installing build-essential cmake vim.. ====="
-if [ ! -z $APT_CMD ]; then 
-	sudo apt update -y && sudo apt upgrade -y
-	sudo add-apt-repository ppa:jonathonf/vim -y
-	sudo apt update -y && sudo apt upgrade -y
-	sudo apt install -y build-essential cmake
+echo "Install necessary utilities"
+sudo apt-get install -y \
+	git \
+	unzip \
+	libxt-dev \
+	build-essential \
+	cmake
 
-	# install vim 8.2
-	sudo apt install -y vim
-	
-	# https://stackoverflow.com/questions/65284572/your-c-compiler-does-not-fully-support-c17
-	sudo apt-get install g++-8
-	sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-7 700 --slave /usr/bin/g++ g++ /usr/bin/g++-7
-	sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-8 800 --slave /usr/bin/g++ g++ /usr/bin/g++-8
-else
-	printf "\nNOT SUPPORT :))"
-	exit 1
-fi
+echo "===== Clone vim and compile from souce ====="
+git clone https://github.com/vim/vim/archive/master.zip
+unzip master.zip
+cd vim-master
+./configure --with-x
+make -j8
+sudo make install
 
-printf "\n===== Copying config files.. ====="
+# # https://stackoverflow.com/questions/65284572/your-c-compiler-does-not-fully-support-c17
+# sudo apt-get install g++-8
+# sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-7 700 --slave /usr/bin/g++ g++ /usr/bin/g++-7
+# sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-8 800 --slave /usr/bin/g++ g++ /usr/bin/g++-8
+
+
+echo "===== Copying config files.. ====="
 cp .bashrc ~/.bashrc
 cp .bash_aliases ~/.bash_aliases
 cp .vimrc ~/.vimrc
@@ -31,23 +34,22 @@ cp .ycm_extra_conf.py ~/.ycm_extra_conf
 cp .inputrc ~/.inputrc
 cp .tmux.conf ~/.tmux.conf
 
-printf "\n===== Copying SSH config file.. ====="
+echo "===== Copying SSH config file.. ====="
 if [ -n -d $HOME/.ssh ]; then
-	echo "~/..ssh does not exists! Create one.."
 	mkdir $HOME/.ssh
 fi
 cp .ssh/config $HOME/.ssh/config
 
-printf "\n===== Setup YouCompleteMe.. ====="
+echo "===== Setup YouCompleteMe.. ====="
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
 cd ~/.vim/bundle/YouCompleteMe
 python3 install.py
 
 
-printf "\n===== Install fzf.. ====="
+echo "===== Install fzf.. ====="
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 bash ~/.fzf/install --all
 
 
-printf "\n==== DONE ===="
+echo "==== DONE ===="
