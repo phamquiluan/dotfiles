2bdede0f6f36703faeb9b3d437cf2a39cc6b2037
Luan Pham <luan@cinnamon.is>
2021-12-08 15:08:49 +0700

    Update sudo_init script

--- sudo_init.sh
+++ sudo_init.sh
@@ -4,16 +4,6 @@ CUR_DIR="$(pwd)"
 
 # Install necessary utilities
 apt-get update -y && apt-get upgrade -y
-apt-get install -y \
-	python3-dev \
-	git \
-	libncurses5-dev \
-	unzip \
-	libxt-dev \
-	libx11-dev \
-	libxtst-dev \
-	build-essential \
-	cmake
 
 # Clone vim and compile from souce
 git clone https://github.com/vim/vim.git
@@ -35,12 +25,6 @@ cp .vimrc ~/.vimrc
 cp .ycm_extra_conf.py ~/.ycm_extra_conf
 cp .inputrc ~/.inputrc
 cp .tmux.conf ~/.tmux.conf
-
-# Copying SSH config file
-if [ -n -d $HOME/.ssh ]; then
-	mkdir $HOME/.ssh
-fi
-cp .ssh/config $HOME/.ssh/config
 
 # Setup YouCompleteMe
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
