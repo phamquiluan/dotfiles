7c3b578ffadb7995002919789dbc2c0ad79a235d
Luan Pham <luan@cinnamon.is>
2023-01-26 22:44:17 +0000

    Add init_codespaces.sh and .gitignore

---
+++ init_codespace.sh
@@ -0,0 +1,21 @@
+sudo apt-get update -y
+
+echo "Copy dotfiles.."
+cp .bashrc ~/.bashrc
+cp .bash_aliases ~/.bash_aliases
+cp .vimrc ~/.vimrc
+cp .ycm_extra_conf.py ~/.ycm_extra_conf
+cp .inputrc ~/.inputrc
+cp .tmux.conf ~/.tmux.conf
+
+echo "Install Vim Plugins.."
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim +PluginInstall +qall
+cd ~/.vim/bundle/YouCompleteMe
+
+python3 install.py --verbose
+
+echo "Install FZF.."
+sudo apt install ripgrep
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+bash ~/.fzf/install --all
